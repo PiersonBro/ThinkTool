a5e84d5bf1fc40f893656f6aba67d43704a762af
Ezekiel Pierson <ezekiel.m.pierson@gmail.com>
2020-12-03 16:56:36 -0800

    Begin to sketch out sign in UI

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -3,23 +3,26 @@
 import React from "react";
 import SaveCustomNote from "../saveCustomNote";
 import DisplayNote from "../displayNote";
+import SignIn from "../signIn";
+import firebase from "firebase"
 
-const AddNote = (databaseref) => {
-	databaseref.child("abc").set({
-		title: "Testing 11/27",
-		user_id: "12345",
-		content: "testing!",
-		noteID: "12345",
-	});
-};
+
+const uiConfig = {
+	// Popup signin flow rather than redirect flow.
+	signInFlow: 'popup',
+	// Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+	signInSuccessUrl: '/signedIn',
+	// We will display Google and Facebook as auth providers.
+	signInOptions: [
+	  firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+	  firebase.auth.FacebookAuthProvider.PROVIDER_ID
+	]
+  };
 
 const Home = ({ databaseref }) => {
 	return (
 		<div>
-			<div>hello</div>
-			<button onClick={() => AddNote(databaseref)}>Save Note</button>
-			<SaveCustomNote databaseref={databaseref} />
-			<DisplayNote databaseref={databaseref} />
+			<SignIn uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>
 		</div>
 	);
 };
