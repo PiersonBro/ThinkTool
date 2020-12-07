14939df9de6ac9a81e20058081f3813bdd4c3c80
Ezekiel Pierson <ezekiel.m.pierson@gmail.com>
2020-12-07 15:02:50 -0800

    It's kinda broken right now!

--- src/components/saveCustomNote/index.jsx
+++ src/components/saveCustomNote/index.jsx
@@ -1,8 +1,40 @@
 /** @format */
 
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import styles from "./styles.css";
 import firebase from "firebase";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import debounce from "lodash.debounce";
+
+const DEBOUNCE_SAVE_DELAY_MS = 1000;
+
+function Autosave ({databaseref, noteID, data}) {
+	console.log("what")
+	// console.log(databaseref);
+	// console.log(noteID);
+	const debouncedSave = useCallback(
+		debounce(async (newData, noteID, ref) => {
+			await databaseref.child(noteID).set({
+				title: newData.title,
+				user_id: firebase.auth().currentUser.uid,
+				content: newData.text,
+				noteID:  noteID,
+			});
+		}, DEBOUNCE_SAVE_DELAY_MS), 
+		[],
+	);
+	
+	useEffect(() => {
+		if (data) {
+			console.log("debouncing!!!");
+			// debouncedSave(data, noteID, databaseref);
+		}
+	}, [data, debouncedSave])
+
+	return null;
+}
+
 
 class Note extends React.Component {
 	// props: {
@@ -12,30 +44,30 @@ class Note extends React.Component {
 	//  noteID;
 	// }
 
+	constructor(props) {
+		console.log("hello!!!!!");
+		super(props);
+		this.saveText = this.saveText.bind(this);
+		this.handleSubmit = this.saveText.bind(this);
+	}
+
 	state = {
 		title: this.props.title,
 		text: this.props.text,
 	};
 
-	SaveTitle = (title) => {
+	saveTitle = (title) => {
+		console.log(title);
+		console.log("triggered!!");
 		this.setState({ title });
 	};
 
-	SaveText = (text) => {
+	saveText = (text) => {
 		this.setState({ text });
 	};
-	// I don't think we should be saving here!
-	// But.... I don't understand react well enough to have anything more than an intuition. 
-	SaveNote = () => {
-		return this.props.databaseref.child(noteID).set({
-			title: this.state.title,
-			user_id: firebase.auth().currentUser.uid,
-			content: this.state.text,
-			noteID:  this.props.noteID,
-		});
-	};
-
+	
 	render() {
+		// console.log(this.props.noteID);
 		return (
 			<React.Fragment>
 				<div className={styles.noteblock}>
@@ -43,11 +75,11 @@ class Note extends React.Component {
 						type='text'
 						value={this.props.title}
 						placeholder="Title"
-						onChange={(event) => this.SaveTitle(event.target.value)}
+						onChange={(event) => this.saveTitle(event.target.value)}
 					/>
 					<hr />
-					<textarea onChange={(event) => this.SaveText(event.target.value)} cols="40" rows="10" value={this.props.text} placeholder="Text"></textarea>
-					<button onClick={this.SaveNote} className={styles.savebutton}>Save Note</button>
+					<textarea onChange={(event) => this.saveText(event.target.value)} cols="40" rows="10" value={this.props.text} placeholder="Text"></textarea>
+					{/* <Autosave databaseref={this.props.databaseref} noteID={this.props.noteID} data={this.state}/> */}
 				</div>
 			</React.Fragment>
 		);
