d0910b1b915bb8935f4b1e9c2a8e955afce5404c
Alyssa Fusato <a.c.fusato@gmail.com>
2020-12-16 18:51:22 -0800

    Fixed styling for note titles

--- src/components/Note/DraggableNode.jsx
+++ src/components/Note/DraggableNode.jsx
@@ -3,13 +3,13 @@ import { DragPreviewImage, useDrag } from 'react-dnd';
 import { ItemTypes } from '../ItemTypes.js';
 import styles from "./styles.css";
 
-export const Box = function ({ name, callback, otherProps, secondCallback}) {
+export const Box = function ({ name, callback, otherProps, secondCallback }) {
     const [{ isDragging }, drag] = useDrag({
         item: { name, otherProps, type: ItemTypes.NOTE },
         end: (item, monitor) => {
             const dropResult = monitor.getDropResult();
             if (item && dropResult) {
-                var randomColor = Math.floor(Math.random()*16777215).toString(16);
+                var randomColor = Math.floor(Math.random() * 16777215).toString(16);
                 secondCallback(dropResult.props.noteID, randomColor);
                 dropResult.callback(item.otherProps.noteID, randomColor);
             }
@@ -21,13 +21,14 @@ export const Box = function ({ name, callback, otherProps, secondCallback}) {
     const opacity = isDragging ? 0.4 : 1;
     return (<>
         <div ref={drag} /*className={styles.noteblock}*/ /*style={{ ...styles.noteblock, opacity }}*/>
-        <input
-						type='text'
-						value={name}
-						placeholder='Title'
-						onChange={(event) => callback(event)}
-					/>
-					<hr />
+            <input
+                className={styles.noteblockInput}
+                type='text'
+                value={name}
+                placeholder='Title'
+                onChange={(event) => callback(event)}
+            />
+            <hr />
         </div>
     </>);
 };
